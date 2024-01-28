df2c1c4314371decb79a3851f13e8f219d2f8b9e
noel <noelantoine974@outlook.fr>
2024-01-28 11:07:37 +0400

    debut

---
+++ sportsee_front/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Styles/Styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SportSee</h1>
+    </div>
+  );
+}
+
+export default App;
