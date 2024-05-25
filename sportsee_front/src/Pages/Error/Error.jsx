ec97cc116b35679f46e162af5ea6d497c39fb1f8
noel <noelantoine974@outlook.fr>
2024-05-25 17:27:23 +0400

    mise a jour css

--- sportsee_front/src/Pages/Error/Error.jsx
+++ sportsee_front/src/Pages/Error/Error.jsx
@@ -4,6 +4,7 @@ function Error() {
         <div className="main">
            <div className='error'><h1>404</h1></div>
            <div className="text"><p>Page non trouvée</p></div>
+           <div className="text1"><p>Erreur du serveur</p></div>
         </div>
         
     );
